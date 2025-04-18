823eb3beb32321fb81761c5fc4967500f1c2c9a5
Douglas Duarte <douglasduarte.eng@gmail.com>
2025-04-18 09:18:36 -0300

    feat(auth): integrate Firebase authentication with auth.js
    Added Firebase authentication setup using auth.js. This includes initializing Firebase Admin SDK with credentials and preparing the authentication flow for the application.

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,12 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter"
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+
+import { firebaseCert } from "@/app/lib/firebase"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google]
+  providers: [Google],
+  adapter: FirestoreAdapter({
+    credential: firebaseCert
+  })
 })
